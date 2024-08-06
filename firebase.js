610f8ec693ca29653df817cb5328e7ff58b08360
mileshall1 <156925206+mileshall1@users.noreply.github.com>
2024-08-06 13:20:01 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -18,7 +18,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const firestore = getFirestore(app);
 
 export{firestore}
