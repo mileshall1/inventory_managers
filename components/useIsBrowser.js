45c82abe72cab98e1ec996d382c21d7c05a983c8
Miles Hall <156925206+mileshall1@users.noreply.github.com>
2025-02-06 11:23:42 -0500

    Update useIsBrowser.js

--- components/useIsBrowser.js
+++ components/useIsBrowser.js
@@ -1,1 +1,15 @@
 
+// useIsBrowser.js
+import { useEffect, useState } from 'react';
+
+const useIsBrowser = () => {
+  const [isBrowser, setIsBrowser] = useState(false);
+
+  useEffect(() => {
+    setIsBrowser(true);
+  }, []);
+
+  return isBrowser;
+};
+
+export default useIsBrowser;
